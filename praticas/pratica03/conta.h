9793fc767db8970f3c03be3eb75fe1d145e767b5
sabinodesousaleda <142452714+sabinodesousaleda@users.noreply.github.com>
2026-04-24 01:40:51 -0300

    Create conta.h

---
+++ praticas/pratica03/conta.h
@@ -0,0 +1,10 @@
+#ifndef CONTA_H
+#define CONTA_H
+
+typedef struct conta_t Conta;
+
+Conta* conta_criar(int numero);
+void conta_depositar(Conta* c, float valor);
+void conta_sacar(Conta* c, float valor);
+float conta_ver_saldo(Conta* c);
+void conta_destruir(Conta* c);
